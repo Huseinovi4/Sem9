c12646d7c2be83a62b36d84b0b480e06875c61b1
Aleksey <lexandreeev@gmail.com>
2023-02-24 22:04:35 +0300

    Добавил задание 4

---
+++ task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите два числа N и M");
+Console.ReadKey();
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int Accerman(int Num1, int Num2)
+{
+  if (Num1 == 0)
+  {
+    return Num2 + 1;
+  }
+  else
+    if ((Num1 != 0) && (Num2 == 0))
+  {
+    return Accerman(Num1 - 1, 1);
+  }
+  else
+  {
+    return (Accerman(Num1 - 1, Accerman(Num1, Num2 - 1)));
+  }
+}
+
+Console.WriteLine();
+Console.Write("Результат функции равен: ");
+Console.Write(Accerman(N, M));
