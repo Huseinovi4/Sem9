6ee3312a0e468a9849119ad16399c069ed90769c
Aleksey <lexandreeev@gmail.com>
2023-02-24 18:30:23 +0300

    Добавил первое задание

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+int PtintNumber(int n)
+{
+  if(n > 1)
+  {
+  Console.WriteLine(n);
+  return PtintNumber(n - 1);
+  }
+  else return 1;
+  
+
+}
+
+System.Console.WriteLine(PtintNumber(N));
